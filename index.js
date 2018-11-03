d931de074418749adaab99be16f0dc0947b78f01
Daniyal Guliev <tzota@yandex-team.ru>
2018-11-03 14:13:44 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
+const {page} = require('./utils');
+
+const records = parse(
+    fs.readFileSync('./input.csv', 'utf-8'),
+    {
+        columns: ['name', 'from', 'due', '_', '_2', '_3', '_4', '_5'],
+        skip_empty_lines: true,
+        cast: (value, {column}) => {
+            if (!['from', 'due'].includes(column)) return value;
+
+            let parts = value.split('/');
+            if (parts.length === 3) {
+                return new Date(parts[2], parts[0] - 1, parts[1]);
+            }
+            parts = value.split('.');
+
+            if (parts.length === 3) {
+                return new Date(parts[2], parts[1] - 1, parts[0]);
+            }
+
+            return '';
+        },
+    },
+);
+
+const [headers, ...data] = records;
+const stop = new Date(headers._);
+const result = page(data, stop);
+
+try {
+    fs.accessSync('./build');
+} catch (error) {
+    fs.mkdirSync('./build');
+}
+
+fs.writeFileSync('./build/index.html', result, 'utf-8');
---
+++ utils.js
@@ -0,0 +1,53 @@
+const moment = require('moment');
+
+const fmtDate = date => date ? moment(date).format('DD.MM.YYYY') : '';
+
+const diff = (from, due, stop) => {
+  if (!from || !due || !stop) return '';
+
+  return `${Math.round(100 * (due-stop)/(due-from))}%`;
+};
+
+// const line = ({name, from, due, stop}) => (due-stop);
+
+const line = ({name, from, due, stop}) => `
+<tr>
+  <td class="name">${name}</td>
+  <td class="from">${fmtDate(from)}</td>
+  <td class="due">${fmtDate(due)}</td>
+  <td class="percent">${diff(from, due, stop)}</td>
+</tr>
+`;
+
+const page = (data, stop) => `
+<html>
+<head>
+  <style>
+    body {font-family: sans-serif}
+    table {width: 100%; border-spacing: 0;}
+    tr:nth-child(odd) {background-color: #cccccc}
+    td {padding: 0.5ex}
+    .from, .due, .percent {white-space: nowrap}
+    .name {width: 100%}
+  </style>
+</head>
+<body>
+<h1>Сроки склад ${fmtDate(stop)}</h1>
+${data.reduce((acc, {name, from, due}) => {
+  if (from === '' && due === '') {
+    acc.push([name, []]);
+  } else {
+    (acc[acc.length - 1][1]).push({name, from, due, stop})
+  }
+  return acc;
+}, [])
+  .map(([name, lines]) => `<h2>${name}</h2><table>${lines.map(line).join('')}</table>`)
+  .join('')
+}
+</body>
+</html>
+`;
+
+module.exports = {
+  page,
+};
